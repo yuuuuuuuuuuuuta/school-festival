fa817747a4a1fc2d7056095c6f5c2ecb0a7f6aa1
RYU <takahashi.ryunosuke.job@gmail.com>
2024-05-27 09:38:40 +0900

    style: scroll down

--- src/app/_components/mobile/scroll-down.tsx
+++ src/app/_components/mobile/scroll-down.tsx
@@ -2,7 +2,7 @@ import styles from './style.module.css'
 
 export default function ScrollDown() {
   return (
-    <div className="fixed bottom-3 right-1/2 -z-10 h-screen translate-x-1/2">
+    <div className="fixed bottom-3 right-0 -z-10 h-screen w-full">
       <div className={styles.scroll_down} id="type07">
         <a href="#main">
           <div className={styles.arrow}></div>
