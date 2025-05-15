5960bd09954558366e1b650ecddacf0facbabf82
Yuta Terada <ttc22c1100014@edu.tech.ac.jp>
2025-05-15 20:01:16 +0900

    chore: initial commit

--- src/app/[buildingId]/_components/floor-list.tsx
+++ src/app/[buildingId]/_components/floor-list.tsx
@@ -25,35 +25,37 @@ export default function FloorList({ buildingId }: { buildingId: string }) {
           </div>
         ))}
       </div>
-      {building.floors.map((floor) => (
-        <div
-          key={floor.id}
-          className="relative aspect-[16/12] w-full border-b first-of-type:border-t"
-          style={{ borderColor: building.accentColor }}
-        >
-          <div className="absolute right-0 top-1/2 w-full -translate-y-1/2">
-            <Image
-              className="!relative w-full object-contain"
-              src={`/images/floors/${floor.id}.svg`}
-              alt={floor.name}
-              fill
+      {[...building.floors]
+        .sort((a, b) => b.number - a.number) //表示階層を降べきの順に
+        .map((floor) => (
+          <div
+            key={floor.id}
+            className="relative aspect-[16/12] w-full border-b first-of-type:border-t"
+            style={{ borderColor: building.accentColor }}
+          >
+            <div className="absolute right-0 top-1/2 w-full -translate-y-1/2">
+              <Image
+                className="!relative w-full object-contain"
+                src={`/images/floors/${floor.id}.svg`}
+                alt={floor.name}
+                fill
+              />
+            </div>
+            <h3
+              className="absolute left-4 top-6 flex size-11 items-center justify-center rounded-full text-xl font-bold text-white sm:left-6 sm:top-10 sm:size-14"
+              style={{
+                backgroundColor: building.accentColor,
+              }}
+            >
+              {floor.name}
+            </h3>
+            <BoothItems
+              booths={floor.booths}
+              themeColor={building.themeColor}
+              accentColor={building.accentColor}
             />
           </div>
-          <h3
-            className="absolute left-4 top-6 flex size-11 items-center justify-center rounded-full text-xl font-bold text-white sm:left-6 sm:top-10 sm:size-14"
-            style={{
-              backgroundColor: building.accentColor,
-            }}
-          >
-            {floor.name}
-          </h3>
-          <BoothItems
-            booths={floor.booths}
-            themeColor={building.themeColor}
-            accentColor={building.accentColor}
-          />
-        </div>
-      ))}
+        ))}
     </div>
   )
 }
